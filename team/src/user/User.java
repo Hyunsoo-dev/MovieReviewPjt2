049c0077261683973930230c3fb9e539f8d82ea3
Sangwook760424 <66587200+Sangwook760424@users.noreply.github.com>
2020-06-24 14:44:02 +0900

    sangwook branch1
    sangwook branch1

---
+++ team/src/user/User.java
@@ -0,0 +1,49 @@
+package user;
+
+public class User {
+	private int userNum;	
+	private int userLevel;	
+	private String userID;
+	private String userPassword;
+	private String userName;
+	private String userEmail;
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public int getUserNum() {
+		return userNum;
+	}
+	public void setUserNum(int userNum) {
+		this.userNum = userNum;
+	}
+	public int getUserLevel() {
+		return userLevel;
+	}
+	public void setUserLevel(int userLevel) {
+		this.userLevel = userLevel;
+	}
+	
+
+}
